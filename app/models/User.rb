e922cfb3caa583fe877fa767f7f417e68a1e43a6
Aaron Na <aaronna@Aarons-MacBook-Air.local>
2018-05-07 16:10:22 -0400

    Created user input method to get array of city & sites.

--- app/models/User.rb
+++ app/models/User.rb
@@ -3,4 +3,23 @@ class User < ActiveRecord::Base
   has_many :cities, through: :usercity
   belongs_to :usersite
   has_many :sites, through: :usersite
+  def get_city_and_type_name
+    save_array = []
+    city_input = gets.chomp
+    coordinate_finder = RestClient.get('https://maps.googleapis.com/maps/api/geocode/json?address="#{input}"&key=AIzaSyAfv9BopUtEDnzyZGBGFTf0xHX5Kz-f2mU')
+    latitude = coordinate_finder[:results][:geometry][:location][:latitude]
+    longitude = coordinate_finder[:results][:geometry][:location][:longitude]
+    type_input = gets.chomp
+    while type_input != "No"
+      puts "Do you want to add more sites?"
+     t = RestClient.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="#{latitude}","#{longitude}"&radius=50000&type="#{type_input}"&key=AIzaSyDXstCAzJeedSOFzmbSigCLDWiEbxnj_CI')
+     t.each do |non_hash|
+       non_hash[:results].select do |result|
+         result[:name]
+       end
+     end
+     save_array <<
+
+    end
+  end
 end
